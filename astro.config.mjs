a6996f500acaa54624887c5ffb00756d853d7ed1
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2023-11-07 13:33:47 -0700

    re-add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from "@astrojs/tailwind";
 // import svelte from "@astrojs/svelte";
 
 // // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
   // integrations: [tailwind(), svelte()]
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()]
 });
